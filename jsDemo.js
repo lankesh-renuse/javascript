cdd4274a848d41b7af71b83f9012ce9c9077809f
lankesh renuse <162421234+lankesh-renuse@users.noreply.github.com>
2024-07-31 18:38:00 +0530

    Add files via upload

---
+++ jsDemo.js
@@ -0,0 +1,11 @@
+//for popup
+alert("welcome to javascript :-");
+
+
+
+//for brwoser
+document.write("<h1 style ='color:red'>welcome to javascript <hr><br>" )
+
+
+//for console
+console.log("welcome to javascript") 
