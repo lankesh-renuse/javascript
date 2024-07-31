3a08b325f26e0132aefee4deb912e8e13d500b10
lankesh renuse <162421234+lankesh-renuse@users.noreply.github.com>
2024-07-31 18:38:22 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+var arr =[ "java" , "c" , "python" ];
+
+console.log(arr);
+console.log(arr[2]);
+
+var fruits = new Array("banana" , "apple" , "orange" , "mango" );
+
+console.log(fruits);
+console.log(fruits[3]);
+
+var city = new Array(5);
+
+city[0]="pune";
+city[1]="pune";
+city[2]="pune";
+city[3]="pune";
+city[4]="pune";
+
+
+document.write(city);
